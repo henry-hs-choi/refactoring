d6f64ad1d230fd4e07d39fed718585e3e20c78f7
danaRyu <dana40426@gmail.com>
2022-04-16 23:05:15 +0900

    refactor: statement의 중첩함수 amountFor 생성 (함수 추출하기)

--- chapter_1/code/statement.js
+++ chapter_1/code/statement.js
@@ -9,6 +9,7 @@ console.log(plays);
 // import ~ from "./invoices.json";
 
 function statement(invoice, plays) {
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = '청구내역 (고객명: ${invocide.customer})\n';
@@ -21,25 +22,8 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
 
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        
-        switch (play.type) {
-        case "tragedy": // 비극
-            thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
-            }
-            break;
+        let thisAmount = amountFor(perf, play);
 
-        case "comedy": // 희극
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 1000 + 500 & (perf.audience - 20);
-            }
-            break;
-        default:
-            throw new Error(`알 수 없는 장르 : ${play.type}`);
-        }
         //포인트를 적립한다
         volumeCredits += Math.max(perf.audience - 30, 0);
         //희극 관객 5명마다 추가 포인트를 제공한다.
@@ -53,6 +37,32 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트: ${volumeCredits}\n`;
     return result;
+}
+
+function amountFor(perf, play) {
+
+        let thisAmount = 0;
+
+        switch (play.type) {
+        case "tragedy": // 비극
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+
+        case "comedy": // 희극
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 1000 + 500 & (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르 : ${play.type}`);
+        }
+
+        return thisAmount;
 }
 
 function main() {
