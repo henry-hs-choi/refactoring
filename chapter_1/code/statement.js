00f9bb4be6628225b157951dc417a0a13a2dfc8d
danaryu <dana40426@gmail.com>
2022-04-17 16:49:47 +0900

    refactor : amountFor 함수 호출부 -> 중간 데이터를 사용하도록 변경

--- chapter_1/code/statement.js
+++ chapter_1/code/statement.js
@@ -14,11 +14,36 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerfomance) {
         const result = Object.assign({}, aPerfomance); // 얕은 복사 수행
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
     
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerfomance) {
+        let result = 0;
+
+        switch (aPerfomance.play.type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (aPerfomance.audience > 30) {
+                    result += 1000 * (aPerfomance.audience - 30);
+                }
+                break;
+            case "comedy": // 희극
+                result = 30000;
+                if (aPerfomance.audience > 20) {
+                    result += 1000 + 500 & (aPerfomance.audience - 20);
+                }
+                result += 300 * aPerfomance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르 : ${aPerfomance.play.type}`);
+        }
+
+        return result;
     }
 }
 
@@ -26,7 +51,7 @@ function renderPlainText(data, plays) {
     let result = `청구내역 (고객명: ${data.customer})\n`;
 
     for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(totalAmount())}\n`;
@@ -37,7 +62,7 @@ function renderPlainText(data, plays) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
 
         return result;
@@ -67,31 +92,6 @@ function renderPlainText(data, plays) {
             volumeCredits += Math.floor(aPerfomance.audience / 5);
 
         return volumeCredits;
-    }
-
-    function amountFor(aPerfomance) {
-
-        let result = 0;
-
-        switch (aPerfomance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerfomance.audience > 30) {
-                    result += 1000 * (aPerfomance.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerfomance.audience > 20) {
-                    result += 1000 + 500 & (aPerfomance.audience - 20);
-                }
-                result += 300 * aPerfomance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${aPerfomance.play.type}`);
-        }
-
-        return result;
     }
 }
 
