84bd28dd2a97482cec73785560b1302d281295d6
danaryu <dana40426@gmail.com>
2022-04-17 16:04:03 +0900

    refactor : 변수 inline

--- chapter_1/code/statement.js
+++ chapter_1/code/statement.js
@@ -14,11 +14,9 @@ function statement(invoice, plays) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    
-    let volumeCredits = totalVolumeCredits();
 
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}\n`;
     return result;
 
     function totalVolumeCredits() {
