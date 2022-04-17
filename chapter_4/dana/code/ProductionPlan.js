4d6c05152e506ac80b1db56f7504a8b8119ce877
danaRyu <dana40426@gmail.com>
2022-04-17 23:37:44 +0900

    feat: 4장 요약, 코드 추가

---
+++ chapter_4/dana/code/ProductionPlan.js
@@ -0,0 +1,122 @@
+var assert = require('assert');
+//var expect = require('expect');
+
+class Province {
+    constructor(doc) {
+        this._name = doc.name;
+        this._producer = [];
+        this._totalProduction = 0;
+        this._demand = doc.demand;
+        this._price = doc.price;
+        doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
+    }
+
+    addProducer(arg) {
+        this._producer.push(arg);
+        this._totalProduction += arg.production;
+    }
+
+    get name() {return this._name};
+    get producer() {return this._producer.slice();}
+    get totalProduction() {return this._totalProduction;}
+    set totalProduction(arg) {this._totalProduction = arg;}
+    get demand() {return this._demand;}
+    set demand(arg) {this._demand = parseInt(arg);}
+    get price() {return this._price;}
+    set price(arg) {this._price = parseInt(arg);}
+
+    get shortfall() {
+        return this._demand - this.totalProduction;
+    }
+    get profit() {
+        return this.demandValue - this.demandCost;
+    }
+
+    get demandValue() {
+        return this.satisfiedDemand * this.price;
+    }
+
+    get satisfiedDemand() {
+        return Math.min(this._demand, this.totalProduction);
+    }
+    
+    get demandCost() {
+        let remainingDemand = this.demand;
+        let result = 0;
+        this.producers
+          .sort((a,b) => a.cost - b.cost)
+          .forEach(p => {
+              const contribution = Math.min(remainingDemand, p.production);
+              remainingDemand -= contribution;
+              result += contribution * p.cost;
+          });
+          return result;
+    }
+
+}
+
+class Producer {
+    constructor(aProvince, data) {
+        this._province = aProvince;
+        this._cost = data.cost;
+        this._name = data.name;
+        this._production = data.production || 0;
+    }
+
+    get name() {return this._name;}
+    get cost() {return this._cost;}
+    set cost(arg) {this._cost = parseInt(arg);}
+
+    get production() {return this._production;}
+    set production(amountStr) {
+        const amount = parseInt(amountStr);
+        const newProduction = Number.isNaN(amount) ? 0 : amount;
+        this._province.totalProduction += newProduction - this._production;
+        this._production = newProduction;
+    }
+
+}
+
+function sampleProvinceData() {
+    return {
+        name: "Asia",
+        producers: [
+            {
+                name: "Byzantium", cost: 10, production: 9
+            },
+            {
+                name: "Attalia", cost: 12, production: 10
+            },
+            {
+                name: "Sinope", cost: 10, production: 6
+            }
+        ],
+        demand: 30,
+        price: 20
+    };
+}
+
+function main() {
+    console.log(sampleProvinceData());
+}
+
+main();
+
+describe('province', function () {
+    it('shortfall', function () {
+        const asia = new Province(sampleProvinceData());
+        assert.equal(asia.shortfall, 5);
+    });
+
+/* 
+    it('shortfall', function () {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.shortfall).equal(5);
+    });
+ */
+    it('profit', function() {
+        const asia = new Province(sampleProvinceData());
+        assert.equal(asia.profit, 230);
+    })
+
+});
