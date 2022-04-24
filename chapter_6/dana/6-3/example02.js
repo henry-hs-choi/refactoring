bda3d0142b70ef025d274a9efcc5e16bfb42bfdc
danaryu <dana40426@gmail.com>
2022-04-24 17:26:43 +0900

    refactor: 클래스 문맥 내 활용

--- chapter_6/dana/6-3/example02.js
+++ chapter_6/dana/6-3/example02.js
@@ -7,9 +7,11 @@ class Order {
     get itemPrice() {return this._data.itemPrice;}
 
     get price() {
-        return this.quantity * this.itemPrice -
-            Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-            Math.min(this.quantity * this.itemPrice * 0.1, 100);
+        return this.basePrice() - this.quantityDiscount() + this.shipping();
+            
     }
 
+    get basesPrice() {return this.quantity * this.itemPrice;}
+    get quantityDiscount() {return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;}
+    get shipping() {return Math.min(this.quantity * this.itemPrice * 0.1, 100);}
 }
