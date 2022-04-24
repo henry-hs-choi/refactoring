af078b1ca14ec373529756b6ec3a009b49e1f227
danaryu <dana40426@gmail.com>
2022-04-24 18:22:38 +0900

    feat: 추가 예시코드04

---
+++ chapter_6/dana/6-5/example04.js
@@ -0,0 +1,5 @@
+const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+
+function inNewEngland(aCustomer) {
+    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+}
