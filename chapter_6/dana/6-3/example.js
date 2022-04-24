76b4da11be27f60138c991130d4fce1c2bca177d
danaryu <dana40426@gmail.com>
2022-04-24 17:19:36 +0900

    refactor: 추출 변수

--- chapter_6/dana/6-3/example.js
+++ chapter_6/dana/6-3/example.js
@@ -1,8 +1,7 @@
 function price(order) {
-    // 가격 (price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
-    // 불변변수 선언 후, 이름을 붙일 표현식의 복제본 대입 및 교체
-    return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(basePrice * 0.1, 100);
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    const shipping = Math.min(basePrice * 0.1, 100);
+    
+    return basePrice - quantityDiscount + shipping;
 }
