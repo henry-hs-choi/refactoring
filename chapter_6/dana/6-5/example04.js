aa6a016d89893f771ece71b41a9fc64f9a283eea
danaryu <dana40426@gmail.com>
2022-04-24 18:27:55 +0900

    refactor : 적용 함수 선언 바꾸기

--- chapter_6/dana/6-5/example04.js
+++ chapter_6/dana/6-5/example04.js
@@ -1,10 +1,10 @@
-const newEnglanders = someCustomers.filter(c => xxNEWinEngland(c.address.state));
+const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
 
-function inNewEngland(aCustomer) {
-    return xxNEWinEngland(aCustomer.address.state);
-}
+// function inNewEngland(aCustomer) {
+//     return xxNEWinEngland(aCustomer.address.state);
+// }
 
-function xxNEWinEngland(stateCode) {
+function inNewEngland(stateCode) {
     return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
