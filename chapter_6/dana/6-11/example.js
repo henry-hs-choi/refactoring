0a8bdba2c6d8104044777863b528e6fbb23e7957
danaRyu <dana40426@gmail.com>
2022-05-02 20:17:40 +0900

    refactor: 첫번째 단계 추출 및 인라이

--- chapter_6/dana/6-11/example.js
+++ chapter_6/dana/6-11/example.js
@@ -1,21 +1,26 @@
 function priceOrder(product, quantity, shippingMethod) {
-    const basePrice = product.basePrice * quantity;
-    const discount = Math.max(quantity - product.discountThreshold, 0)
-            * product.basePrice * product.discountRate; // 상품 가격 계산
-
-    // 첫 번째 단계와 두 번째 단계가 주고받을 중간 데이터 구조 생성
-    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
-    const price = applyShipping(priceData, basePrice, shippingMethod);
-    return price;
+    const priceData = calculatePricingData(product, quantity);
+    return applyShipping(priceData, basePrice, shippingMethod);
 }
 
 /**
- * 배송비 계산 부분 함수 추출
+ * 가격 계산 함수 (첫 번째 단계)
+ */
+function calculatePricingData(product, quantity) {
+
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0)
+            * product.basePrice * product.discountRate; // 상품 가격 계산
+    return {basePrice: basePrice, quantity: quantity, discount: discount};
+}
+
+/**
+ * 배송비 계산 부분 함수 추출 (두 번째 단계)
  */
 function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
             ? shippingMethod.discountedFee : shippingMethod.feePerCase; // 배송비 계산
     const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
-    return price;
+    return basePrice - discount + shippingCost;
 }
+
