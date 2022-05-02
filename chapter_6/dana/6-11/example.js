4ea78ec26227d9c8da76a9abec39e7c947c5ef4d
danaRyu <dana40426@gmail.com>
2022-05-02 19:56:20 +0900

    refactor : 추출 배송비 계산 함수

--- chapter_6/dana/6-11/example.js
+++ chapter_6/dana/6-11/example.js
@@ -4,6 +4,18 @@ function priceOrder(product, quantity, shippingMethod) {
             * product.basePrice * product.discountRate; // 상품 가격 계산
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
             ? shippingMethod.discountedFee : shippingMethod.feePerCase; // 배송비 계산
+    const shippingCost = quantity * shippingPerCase;
+    const price = basePrice - discount + shippingCost;
+    return price;
+}
+
+/**
+ * 배송비 계산 부분 함수 추출
+ */
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+            ? shippingMethod.discountedFee : shippingMethod.feePerCase; // 배송비 계산
+    const shippingCost = quantity * shippingPerCase;
     const price = basePrice - discount + shippingCost;
     return price;
 }
