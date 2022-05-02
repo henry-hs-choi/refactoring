28dd3a670972c31c5d0b37087015d0b15abf2a56
danaRyu <dana40426@gmail.com>
2022-05-02 19:58:00 +0900

    refactor: 생성 중간 데이터 구조

--- chapter_6/dana/6-11/example.js
+++ chapter_6/dana/6-11/example.js
@@ -2,17 +2,17 @@ function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
             * product.basePrice * product.discountRate; // 상품 가격 계산
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
-            ? shippingMethod.discountedFee : shippingMethod.feePerCase; // 배송비 계산
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
+
+    // 첫 번째 단계와 두 번째 단계가 주고받을 중간 데이터 구조 생성
+    const priceDate = {};
+    const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
     return price;
 }
 
 /**
  * 배송비 계산 부분 함수 추출
  */
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
             ? shippingMethod.discountedFee : shippingMethod.feePerCase; // 배송비 계산
     const shippingCost = quantity * shippingPerCase;
