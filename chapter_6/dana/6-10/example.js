b30b68c98bb9a7896059de5fb490628ee2c703f1
danaRyu <dana40426@gmail.com>
2022-05-02 19:42:42 +0900

    refactor: 새로만든 필드 인라인

--- chapter_6/dana/6-10/example.js
+++ chapter_6/dana/6-10/example.js
@@ -2,12 +2,13 @@ reading = {customer: "ivan", quantity: 10, month: 5, year: 2017};
 
 // client 1
 const rawReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity
+const aReading = enrichReading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 // client 2
-const aReading = acquireReading();
-const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const taxableCharge = aReading.taxableCharge;
 
 // client 3
 const rawReading = acquireReading();
@@ -21,6 +22,7 @@ function calculateBaseCharge(aReading) {
 function enrichReading(original) {
     const result = _.cloneDeep(original);
     result.baseCharge = calculateBaseCharge(result);
+    result.taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
     return result;
 }
 
