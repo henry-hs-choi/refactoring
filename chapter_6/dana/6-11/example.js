a670beb0356489606f0a1000c3a6bae31725ee70
danaRyu <dana40426@gmail.com>
2022-05-02 19:59:50 +0900

    refactor: 처리 매개변수 (중간 데이터 구조로 이관)

--- chapter_6/dana/6-11/example.js
+++ chapter_6/dana/6-11/example.js
@@ -4,15 +4,15 @@ function priceOrder(product, quantity, shippingMethod) {
             * product.basePrice * product.discountRate; // 상품 가격 계산
 
     // 첫 번째 단계와 두 번째 단계가 주고받을 중간 데이터 구조 생성
-    const priceDate = {};
-    const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
+    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+    const price = applyShipping(priceData, basePrice, shippingMethod);
     return price;
 }
 
 /**
  * 배송비 계산 부분 함수 추출
  */
-function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
             ? shippingMethod.discountedFee : shippingMethod.feePerCase; // 배송비 계산
     const shippingCost = quantity * shippingPerCase;
