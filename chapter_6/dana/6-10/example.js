37c668e0f8bac39355e777143262f3fa4a4e357d
danaRyu <dana40426@gmail.com>
2022-05-02 19:40:02 +0900

    refactor: 변환 함수 생성

--- chapter_6/dana/6-10/example.js
+++ chapter_6/dana/6-10/example.js
@@ -10,10 +10,17 @@ const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
 
 // client 3
-const aReading = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading);
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const basicChargeAmount = aReading.baseCharge;
 
 function calculateBaseCharge(aReading) {
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+function enrichReading(original) {
+    const result = _.cloneDeep(original);
+    result.baseCharge = calculateBaseCharge(result);
+    return result;
 }
 
